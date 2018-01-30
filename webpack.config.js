e4661eba4e84bf3bfa3676677ebca0a247996bf8
Kiv Lo <kivlokw@gmail.com>
2018-01-30 11:35:29 -0500

    set up webpack for project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            // presets option to build file to satisfy browser version
+            ['env', {targets: { browser: ['last 2 versions'] }}]
+          ]
+        }
+      }
+    ]
+  }
+};
