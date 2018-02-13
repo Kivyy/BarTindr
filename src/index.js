957d6d216cf6f0a4a481a60272aa69e5c93130fa
Kiv Lo <kivlokw@gmail.com>
2018-02-13 14:45:33 -0500

    add script to use nodemon to run client server and webpack watching. Also fix the issue where react is not recognizing root container as DOM element

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 // React Components
 import App from './components/App'
 
-ReactDOM.render(<App/>, document.querySelector('.root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
