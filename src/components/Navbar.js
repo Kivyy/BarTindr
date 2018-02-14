2da8ed43180e1e0a015a426abd7fd98feb4c48db
Kiv Lo <kivlokw@gmail.com>
2018-02-14 14:45:33 -0500

    add test component to render in router and making sure react is working correctly.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Navbar extends Component{
   render(){
     return(
       <div>
-        <a href="/">Home</a>
+        <Link to='/'>Home</Link>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-
 // React Components
 import App from './components/App'
-
+import Navbar from './components/Navbar';
+import Test from './components/Test';
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
+      <Navbar/>
       <Route exact path="/" component={App}/>
+      <Route path='/test' component={Test}/>
     </div>
   </BrowserRouter>
 ,document.getElementById('root')
